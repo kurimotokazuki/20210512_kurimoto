bae477a4dcc3f35dbfe1a442f7bc7a314a45d5e5
kazukikurimoto <kurik6548@gmail.com>
2021-05-12 21:11:33 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,142 @@
+<?php get_header(); ?>
+  <div class="top-eyecatch"></div>
+  <main>
+    <div class="flex">
+      <div class="container-top">
+        <div class="new-articles">
+          <h2 class="section-title">NEW ARTICLES</h2>
+          <div class="flex">
+            <a href="" class="magazine-colum">
+              <img src="./img/article_article.jpeg" alt="no-img" />
+              <p class="category-tag">
+                カテゴリー
+              </p>
+              <div class="text-content">
+                <p class="article__date">2020/01/01</p>
+                <h3 class="article__title">
+                  記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                </h3>
+                <div class="article-tags">
+                  <p class="article-tags__inner">
+                    <span class="tag">タグ名</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+            <a href="" class="magazine-colum">
+              <img src="./img/article_article.jpeg" alt="no-img" />
+              <p class="category-tag">
+                カテゴリー
+              </p>
+              <div class="text-content">
+                <p class="article__date">2020/01/01</p>
+                <h3 class="article__title">
+                  記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                </h3>
+                <div class="article-tags">
+                  <p class="article-tags__inner">
+                    <span class="tag">タグ名</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+            <a href="" class="magazine-colum">
+              <img src="./img/article_article.jpeg" alt="no-img" />
+              <p class="category-tag">
+                カテゴリー
+              </p>
+              <div class="text-content">
+                <p class="article__date">2020/01/01</p>
+                <h3 class="article__title">
+                  記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                </h3>
+                <div class="article-tags">
+                  <p class="article-tags__inner">
+                    <span class="tag">タグ名</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="category-sec">
+          <h2 class="section-title">INTERVIEW</h2>
+          <div class="flex">
+            <a href="" class="magazine-colum top-interview">
+              <img src="./img/article_article.jpeg" alt="no-img" />
+              <p class="category-tag">カテゴリー</p>
+              <div class="text-content">
+                <p class="article__date">2020/01/01</p>
+                <h3 class="article__title">
+                  記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                </h3>
+                <div class="article-tags">
+                  <p class="article-tags__inner">
+                    <span class="tag">タグ名</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+            <a href="" class="magazine-colum top-interview">
+              <img src="./img/article_article.jpeg" alt="no-img" />
+              <p class="category-tag">カテゴリー</p>
+              <div class="text-content">
+                <p class="article__date">2020/01/01</p>
+                <h3 class="article__title">
+                  記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                </h3>
+                <div class="article-tags">
+                  <p class="article-tags__inner">
+                    <span class="tag">タグ名</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <p class="news-articles_link">
+            <a href="" class="news-articles_link_text">インタビューの一覧はこちら→</a>
+          </p>
+        </div>
+        <div id="news" class="wrap">
+          <h2 class="section-title">BRAND NEWS</h2>
+          <div class="news-contain">
+            <ul>
+              <li class="news-list">
+                <p class="news-date">2020/01/01</p>
+                <p class="news-category">
+                  new
+                </p>
+                <a href="" class="news-title">
+                  こんにちはニュースのタイトルです。
+                </a>
+              </li>
+              <li class="news-list">
+                <p class="news-date">2020/01/01</p>
+                <p class="news-category">
+                  new
+                </p>
+                <a href="" class="news-title">
+                  こんにちはニュースのタイトルです。
+                </a>
+              </li>
+              <li class="news-list">
+                <p class="news-date">2020/01/01</p>
+                <p class="news-category">
+                  new
+                </p>
+                <a href="" class="news-title">
+                  こんにちはニュースのタイトルです。
+                </a>
+              </li>
+            </ul>
+            <p class="news-articles_link">
+              <a href="" class="news-articles_link_text">ニュース一覧はこちら→</a>
+            </p>
+          </div>
+        </div>
+      </div>
+     <?php get_sidebar(); ?>
+    </div>
+  </main>
+  <?php get_footer(); ?>
+  
